35849fe0047536ee66ac01d193f18ca5543af1f3
crispy chili <zeh1xz@gmail.com>
2020-10-18 22:36:19 -0700

    fixed another bug in model

--- api/models.py
+++ api/models.py
@@ -7,19 +7,19 @@ class Users(models.Model):
     email = models.TextField(max_length = 320, unique = True)
     password = models.CharField(max_length = 100)
     joined = models.DateTimeField(auto_now_add = True)
-    post_count = models.IntegerField(null = True)
+    post_count = models.IntegerField(default = 0)
 
 class Posts(models.Model):
     title = models.TextField(max_length = 250, null = False)
     body = models.TextField(max_length = 500, null = False)
     date = models.DateTimeField(auto_now_add = True)
+    upvotes = models.IntegerField(default = 0)
+    downvotes = models.IntegerField(default = 0)
     user_id = models.ForeignKey('Users', on_delete = models.CASCADE)
-    upvotes = models.IntegerField(null = True)
-    downvotes = models.IntegerField(null = True)
 
 class Replies(models.Model):
     body = models.TextField(max_length = 500, null = False)
+    date = models.DateTimeField(auto_now_add = True)
     user_id = models.ForeignKey('Users', on_delete = models.CASCADE)
     post_id = models.ForeignKey('Posts', on_delete = models.CASCADE)
-    date = models.DateTimeField(auto_now_add = True)
 
