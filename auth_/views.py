194f4bd436d7a35b64216cb541bd755bdbd2edd2
crispy chili <zeh1xz@gmail.com>
2020-10-18 23:31:39 -0700

    implementing auth views

--- auth_/views.py
+++ auth_/views.py
@@ -1,4 +1,44 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
 
+# csrf exempt for api testing
+from django.views.decorators.csrf import csrf_exempt
+
+
+
+# REMOVE DECORATOR FOR PRODUCTION
+@csrf_exempt
+def login(request):
+    
+    # login takes in user and pw in http body as json
+    # validate username and pw
+    # return token in json in http body, or return empty string if invalid
+
+    # login should only accept post calls
+    # TODO: return a proper http response code
+    if request.method != 'POST':
+        return HttpResponse('error')
+
+    body = request.body
+
+    return HttpResponse(body)
+#
+
+
+
+# REMOVE DECORATOR FOR PRODUCTION
+@csrf_exempt
+def signup(request):
+
+    # signup takes in user pw and email in http body as json
+    # validate user for uniqueness
+    # create user in db
+    # return token if success, else return empty string if invalid
+    
+    return HttpResponse('hi')
+#
+
+
+
+# q = request.GET.get('q')
